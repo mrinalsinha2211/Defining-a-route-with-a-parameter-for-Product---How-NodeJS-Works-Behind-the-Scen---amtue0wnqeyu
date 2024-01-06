3aac2e795435fa1d19983c3daf9a6d0834f13caf
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-06 12:34:38 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const port=3000;
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
-  const postId=req.params.productId;
-  res.end(` "productId": "${postId}"`);
+  const productId=req.params.productId;
+   res.status(200).json({
+    "productId": productId
+   })
+});
+app.listen(port,()=>{
+  console.log(`Server is running on http://localhost:${port}`); 
 });
 
 module.exports = app;
